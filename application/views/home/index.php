2d5fee3a9f992dc70cf2db526306ea34eb22f98b
alisofyan <muhammadalisofyan.mail@gmail.com>
2020-07-19 14:12:09 +0700

    create skeleton & styling homepage

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,2 +1,105 @@
-<body class="index">
-    <h1>Hallo, dunia!</h1>
+<div class="container-fluid home">
+    <div class="page-main">
+        <!-- info -->
+        <div class="info">        
+            <h1>Sistem Pendukung Keputusan Pemilihan Laptop Gaming</h1>
+        </div>
+        <!-- alter added -->
+        <div class="after-input">
+            <ul class="list-group">
+                <li class="list-group-item">
+                    <span>ASUS ROG STRIX GL503GE</span>
+                    <a href=""  data-toggle="tooltip" data-placement="top" title="Hapus alternatif" ><i class="fas fa-minus-circle"></i></a>
+                </li>
+                <li class="list-group-item">
+                    <span>ASUS ROG STRIX GL503GE</span>
+                    <a href=""  data-toggle="tooltip" data-placement="top" title="Hapus alternatif" ><i class="fas fa-minus-circle"></i></a>
+                </li>
+            </ul>
+        </div>
+        <!-- button action-->
+        <div class="action-button">
+            <button type="button" class="main-btn-rect popup-btn" data-popup="popup-reg">Tambah Alternif</button>
+            <a href="<?= base_url(); ?>hasil" type="button" class="main-btn-rect action-hitung">HITUNG</a>
+        </div>
+
+    </div>
+
+
+
+    <!-- Popup add Alter -->
+    <div id="popup-reg" class="popup alter">
+        <div class="popup-content">
+            <div class="event-header">
+                <h6>Masukan Spesifikasi Laptop</h6>
+            </div>
+            <hr>
+            <form id="send" class="send-form">
+                <div class="form-group">
+                    <input type="text" placeholder="Masukan merk dan seri laptop" id="merk" name="merk" required="required">
+                    <label for="merk">
+                        <span>Merk</span>
+                    </label>
+                </div>
+              
+                <div class="form-group">
+                    <input type="number" placeholder="Masukan dalam rupiah" id="harga" name="harga" required="required">
+                    <label for="harga">
+                        <span>Harga</span>
+                    </label>
+                </div>
+               
+                <div class="form-group">    
+                    <select class="custom-select" id="vga">
+                        <option selected>Pilih VGA</option>
+                        <option value="2">2 GB</option>
+                        <option value="4">4 GB</option>
+                        <option value="8">8 GB</option>
+                        <option value="16">16 GB</option>
+                        <option value="32">32 GB</option>
+                    </select>
+                    <label class="label vga" for="vga">VGA</label>
+                </div>
+                
+                <div class="form-group">    
+                    <select class="custom-select" id="ram">
+                        <option selected>Pilih RAM</option>
+                        <option value="2">2 GB</option>
+                        <option value="4">4 GB</option>
+                        <option value="8">8 GB</option>
+                        <option value="16">16 GB</option>
+                        <option value="32">32 GB</option>
+                    </select>
+                    <label class="label ram" for="ram">RAM</label>
+                </div>
+
+                <div class="form-group">    
+                    <select class="custom-select" id="processor">
+                        <option selected>Pilih Processor</option>
+                        <option value="3">core i3</option>
+                        <option value="5">core i5</option>
+                        <option value="7">core i7</option>
+                        <option value="9">core i9</option>
+                    </select>
+                    <label class="label processor" for="processor">Processor</label>
+                </div>
+
+                <div class="form-group">
+                    <input type="number" placeholder="Masukan dalam mAh" id="baterai" name="baterai" required="required">
+                    <label for="baterai">
+                        <span>Baterai</span>
+                    </label>
+                </div>
+
+                <div class="form-group action">
+                    <button type="submit" class="main-btn-rect" name="text" value="Send">
+                    <i class="fa fa-paper-plane"></i>Send</button>
+                </div>
+            </form>
+            <span class="fade-out main-btn-circle">╳</span>
+        </div>
+    </div>
+    <!--End Popup add Alter -->
+
+
+</div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,12 +8,22 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="<?= base_url(); ?>assets/vendor/bootstrap/css/bootstrap.min.css">
 
-    <!-- FontAwesome -->
+    <!-- Font -->
     <link rel="stylesheet" href="<?= base_url(); ?>assets/vendor/fontawesome/css/all.css">
     <link rel="stylesheet" href="<?= base_url(); ?>assets/vendor/fontawesome/css/all.min.css">
-    
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+    <!-- style -->
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/app/css/style.css">
+
+
     <!-- Title -->
     <title>Sistem Pendukung Keputusan Pemilihan Laptop</title>
   </head>
 
-
+  <body>
+  
+  <!-- Navbar-->
+  <nav class="navbar">
+    <span class="navbar-brand mb-0 h1">Navbar</span>
+  </nav>
